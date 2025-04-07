1e5183a9baf3432af28bf224c0e897249f5ab3fa
ChoSooah <love2006myg@gmail.com>
2025-04-07 23:59:00 +0900

    5장 아이템 입수 파트 에러

--- 3D Action Game/Assets/Script/Player.cs
+++ 3D Action Game/Assets/Script/Player.cs
@@ -6,6 +6,17 @@ public class Player : MonoBehaviour
     public GameObject[] weapons;
     public bool[] hasWeapons;
 
+    public int ammo;
+    public int coin;
+    public int health;
+    public int hasGrenades;
+
+    public int maxAmmo;
+    public int maxCoin;
+    public int maxHealth;
+    public int maxHasGrenades;
+
+
     float hAxis;
     float vAxis;
 
@@ -174,6 +185,39 @@ public class Player : MonoBehaviour
         {
             anim.SetBool("isJump", false);
             isJump = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            Item item = other.GetComponent<Item>();
+            switch (item.type)
+            {
+                case Item.Type.Ammo:
+                    ammo += item.value;
+                    if (ammo > maxAmmo)
+                        ammo = maxAmmo;
+                    break;
+                case Item.Type.Coin:
+                    coin += item.value;
+                    if (coin > maxCoin)
+                        coin = maxCoin;
+                    break;
+                case Item.Type.Heart:
+                    health += item.value;
+                    if (health > maxHealth)
+                        health = maxHealth;
+                    break;
+                case Item.Type.Grenade:
+                    if (hasGrenades == maxHasGrenades)
+                        return;
+                    hasGrenades[hasGrenades].setActive(true);
+                    hasGrenades += item.value;
+                    break;
+            }
+            Destroy(other.gameObject);
         }
     }
 
